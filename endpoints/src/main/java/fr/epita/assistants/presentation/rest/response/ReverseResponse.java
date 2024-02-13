6513b855c6aee2242c3c72ad72d3accf0f5a4cb2
Jules Raitiere-Delsupexhe <jules.raitiere-delsupexhe@epita.fr>
2024-02-13 15:14:28 +0100

    psg

--- endpoints/src/main/java/fr/epita/assistants/presentation/rest/response/ReverseResponse.java
+++ endpoints/src/main/java/fr/epita/assistants/presentation/rest/response/ReverseResponse.java
@@ -8,13 +8,10 @@ import lombok.NoArgsConstructor;
 public class ReverseResponse {
     public String reverse;
     public String original;
-    public ReverseResponse(String original)
-    {
+
+    public ReverseResponse(String original) {
         this.original = original;
-        StringBuilder r = new StringBuilder();
-        for (int i = 1; i <= original.length(); i++) {
-            r.append(original.charAt(original.length() - i));
-        }
-        this.reverse = r.toString();
+        StringBuilder r = new StringBuilder(original);
+        this.reverse = r.reverse().toString();
     }
 }
